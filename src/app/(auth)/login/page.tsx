d134c57382d857c81018c7f0e89b14f26ba8121a
rahdeg <walett95@gmail.com>
2023-04-25 10:51:52 +0100

    update

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,6 +3,7 @@ import Button from '@/components/ux/Button';
 import React, { FC, useState } from 'react'
 import {signIn} from "next-auth/react"
 import { toast } from 'react-hot-toast';
+import { Icons } from '@/components/Icons';
 
 
 
@@ -27,7 +28,7 @@ const loginWithGoogle = async ()=>{
   <div className=' flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 '>
   <div className='w-full flex flex-col items-center max-w-md space-y-8'>
     <div className=' flex flex-col items-center gap-8'>
-     Logo
+    <Icons.Logo className='h-8 w-auto text-indigo-600' />
      <h2 className=' mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>
       Sign in to your account
       </h2>
--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,25 @@
+import { getFriendsByUserId } from '@/helpers/get-friends-by-user-id';
+import { fetchRedis } from '@/helpers/redis';
+import { authOptions } from '@/lib/auth';
+import { chatHrefConstructor } from '@/lib/utils';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
 import React, { FC } from 'react'
 
 
 
 const page = async ({}) => {
 
+  const session = await getServerSession(authOptions);
+  if(!session)  notFound();
+  const friends = await getFriendsByUserId(session.user.id)
+
+  const friendsWithLastMessage = await Promise.all(
+    friends.map(async(friend)=>{
+      const [lastMessage] = await fetchRedis('zrange',`chat:${chatHrefConstructor(session.user.id,friend.id)}:message`,-1,-1) as Message[]
+      console.log('lastMessage',lastMessage)
+    })
+  )
 
   return <div>Dashboard</div>
 }

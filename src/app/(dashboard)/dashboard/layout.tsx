37e7a84b11f84d1fc3858d2c523ef4ec74643fe6
rahdeg <walett95@gmail.com>
2023-04-25 14:48:00 +0100

    done

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import FriendRequestSidebarOption from '@/components/FriendRequestSidebarOption'
 import { Icon, Icons } from '@/components/Icons'
+import MobileChatLayout from '@/components/MobileChatLayout'
 import SibeBarChatList from '@/components/SibeBarChatList'
 import SignOutButton from '@/components/SignOutButton'
 import { getFriendsByUserId } from '@/helpers/get-friends-by-user-id'
@@ -48,7 +49,10 @@ const layout = async ({children }:layoutProps) => {
 
   return (
     <div className='w-full flex h-screen'>
-    <div className=' flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
+      <div className=' md:hidden'>
+        <MobileChatLayout friends={friends} session={session} sidebarOptions={sidebarOption} unseenRequestCount={unseenRequestCount}/>
+      </div>
+    <div className='hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
     <Link href='/dashboard' className='flex h-16  shrink-0 items-center'>
         <Icons.Logo className='h-8 w-auto text-indigo-600'/>
     </Link>
